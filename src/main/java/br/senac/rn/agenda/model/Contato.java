218662e1f0e069d254e34ad5323c6185670d4292
LUCAS MATHEUS ARAUJO CUNHA <a9260064@rn.senac.br>
2019-10-09 19:15:17 -0300

    add prod e dev

--- src/main/java/br/senac/rn/agenda/model/Contato.java
+++ src/main/java/br/senac/rn/agenda/model/Contato.java
@@ -1,11 +1,6 @@
 package br.senac.rn.agenda.model;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,18 +12,13 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_contatos")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+@Data
 public class Contato {
 
     @Id
     @Column(name = "con_id")
-    @SequenceGenerator(name="tb_contatos_id_sequence", sequenceName="tb_contatos_id_sequence", allocationSize=1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_contatos_id_sequence")
+    //@SequenceGenerator(name="tb_contatos_id_sequence", sequenceName="tb_contatos_id_sequence", allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "con_nome", nullable = false)
